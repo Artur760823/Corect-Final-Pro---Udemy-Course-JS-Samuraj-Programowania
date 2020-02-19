da309b859cc81c334b06e857fecee47a4999e22b
Web Hare <artur.zajaczkowski@icloud.com>
2020-02-19 16:51:29 +0100

    add wallet method

--- Wallet.js
+++ Wallet.js
@@ -12,6 +12,21 @@ class Wallet {
       }
       return false;
     }
+    this.changeWallet = (value, type = '+') => {
+      if (typeof value === "number" && !isNaN(value)) {
+        if (type === "+") {
+          return _money += value;
+        } else if (type === "-") {
+          return _money -= value;
+        } else {
+          throw new Error("Nieprawidłowy typ działania!")
+        }
+
+      } else {
+        console.log(typeof value);
+        throw new Error("Nieprawidłowa liczba!")
+      }
+    }
   }
 }
 const wallet = new Wallet(200);
